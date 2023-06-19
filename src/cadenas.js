97a7b6e1d26b97c8639f83dd14d3d7a0e6fd5567
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-06-19 15:11:03 -0400

    deberia recibir una frase con 2 pañabras y devolver 2

--- src/cadenas.js
+++ src/cadenas.js
@@ -1,7 +1,30 @@
 function ocurrencias(frase) {
-    return 1;
+    frase = frase.toLowerCase();
+    // Definir los caracteres de separación entre palabras
+    const separadores = /[.,;\- ]+/;
+    // Dividir la frase en palabras utilizando los separadores
+    const palabras = frase.split(separadores);
   
-}
+    // Crear un objeto para almacenar el conteo de ocurrencias
+    const conteo = {};
+  
+    // Recorrer todas las palabras y realizar el conteo
+    palabras.forEach(palabra => {
+      // Ignorar las palabras vacías
+      if (palabra !== "") {
+        // Incrementar el conteo de la palabra actual
+        if (conteo[palabra]) {
+          conteo[palabra]++;
+        } else {
+          conteo[palabra] = 1;
+        }
+      }
+    });
+  
+    // Retornar el objeto con el conteo de ocurrencias
+    return conteo;
+  }
+  
   
   module.exports = {ocurrencias};
   
--- src/cadenas.test.js
+++ src/cadenas.test.js
@@ -2,6 +2,11 @@ import {ocurrencias}from "./cadenas.js";
 
 describe("Contar Palabras", () => {
   it("deberia recibir una palabra y devolver 1", () => {
-    expect(ocurrencias("tu")).toEqual(1);
+    expect(ocurrencias("tu")).toEqual({ tu: 1 });
   });
+  it("deberia recibir una Frase con palabras separadas por comas", () => {
+    expect(ocurrencias("tu,hola")).toEqual({ tu: 1,hola:1 });
+  });
+  
+  
 });
